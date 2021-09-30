82e3b80ee3d71a8596c13c558431f5ae77541821
lukemunsch <lpmunsch@gmail.com>
2021-09-30 14:24:48 +0000

    add new code to retieve last 5 sales entries

--- run.py
+++ run.py
@@ -80,6 +80,24 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
+def get_last_5_entries_sales():
+    """
+    collects columns of data from sales worksheet, collecting
+    the last 5 entries for each sandwich and returns the data
+    as a list of lists.
+    """
+    sales = SHEET.worksheet("sales")
+    #column = sales.col_values(3)
+    #print(column)
+
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+    pprint(columns)
+
+
+
 def main():
     """
     run all programme functions
@@ -92,6 +110,6 @@ def main():
 
 
 print("welcome to Love Sandwiches Data Autmoation!\n")
-main()
+# main()
 
-
+get_last_5_entries_sales()
