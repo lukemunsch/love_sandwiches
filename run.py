399d5b83dff112a22a5dd06358681bc0e108a377
lukemunsch <lpmunsch@gmail.com>
2021-09-30 10:13:43 +0000

    add code to get input from user

--- run.py
+++ run.py
@@ -12,8 +12,17 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-sales = SHEET.worksheet('sales')
+def get_sales_data():
+    """
+    get sales info from user input
+    """
+    print("Please enter sales data from the last day of market.")
+    print("Data should be in the format of of 6 number, commas with no space between each.")
+    print("ex. 33,23,24,45,56,52\n")
 
-data = sales.get_all_values()
+    data_str = input("Enter your figures here: ")
+    print(f"The data provided is {data_str}")
 
-print(data)
+
+get_sales_data()
+
